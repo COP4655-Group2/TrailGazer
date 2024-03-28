5e93e3b29b0117dbf26e321e9c71f18ee22a7d65
Seb Barnes <sebastianjbarnes@gmail.com>
2024-03-28 15:43:41 -0400

    Added location info to allow simulation

--- TrailGazerSwift/lab-insta-parse/Home/HomeViewController.swift
+++ TrailGazerSwift/lab-insta-parse/Home/HomeViewController.swift
@@ -39,4 +39,26 @@ class HomeViewController: UIViewController, MKMapViewDelegate, CLLocationManager
             isInitialLocation = false
         }
     }
+    
+    @IBAction func onLogOutTapped(_ sender: Any) {
+        showConfirmLogoutAlert()
+    }
+
+    private func showConfirmLogoutAlert() {
+        let alertController = UIAlertController(title: "Log out of your account?", message: nil, preferredStyle: .alert)
+        let logOutAction = UIAlertAction(title: "Log out", style: .destructive) { _ in
+            NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(logOutAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+
+    private func showAlert(description: String? = nil) {
+        let alertController = UIAlertController(title: "Oops...", message: "\(description ?? "Please try again...")", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
 }
